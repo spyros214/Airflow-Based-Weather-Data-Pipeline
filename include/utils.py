0de62fd480d4cd8a4aaf519d94e42c911d95a8f5
spyros214 <239028416+spyros214@users.noreply.github.com>
2025-11-02 23:13:24 +0200

    Update utils.py

--- include/utils.py
+++ include/utils.py
@@ -6,4 +6,5 @@ ATHENS_TZ = tz.gettz("Europe/Athens")
 
 
 def to_athens(dt: datetime) -> datetime:
+"""Μετατρέπει ένα datetime αντικείμενο στη ζώνη ώρας της Αθήνας."""    
     return dt.astimezone(ATHENS_TZ)
